fcc2be164924890238ac8b946e360345624b2fad
tmokita <tmokita@gmail.com>
2019-04-09 17:33:09 +0900

    practice

---
+++ TodoInput.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    padding: 20,
+  },
+  textInput: {
+    flex: 3,
+    backgroundColor: '#FFF',
+    marginRight: 5,
+  },
+  button: {
+    flex: 1,
+    backgroundColor: '#008080',
+    marginLeft: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontWeight: '500',
+  }
+});
+
+export default class TodoInput extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput style={styles.textInput}/>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>追加</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
